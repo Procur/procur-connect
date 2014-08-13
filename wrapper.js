5159da13d6ed2582f7296539ee7cfddc5a3b430c
Will Ricketts <will@willricketts.com>
2014-08-13 16:50:01 -0400

    added admin fetch methods

--- wrapper.js
+++ wrapper.js
@@ -2,8 +2,56 @@ var host = 'https://api.procur.com';
 
 module.exports = {
 
-  findUser: function(token, userid, callback){
+  //NONSPECIFIC FETCH METHODS
 
+  getUser: function(token, userid, callback) {
+
+  },
+
+  getCompany: function(token, companyId, callback) {
+
+  },
+
+  getBuyer: function(token, buyerId, callback) {
+
+  },
+
+  getSupplier: function(token, supplierId, callback) {
+
+  },
+
+  getLocation: function(token, locationId, callback) {
+
+  },
+
+  getAsset: function(token, assetId, callback) {
+
+  },
+
+  //ADMIN FETCH METHODS
+
+  getAllUsers: function(token, callback) {
+
+  },
+
+  getAllCompanies: function(token, callback) {
+
+  },
+
+  getAllBuyers: function(token, callback) {
+
+  },
+
+  getAllSuppliers: function(token, callback) {
+
+  },
+
+  getAllLocations: function(token, callback) {
+
+  },
+
+  getAllAssets: function(token, callback) {
+    
   }
 
-}
+};
